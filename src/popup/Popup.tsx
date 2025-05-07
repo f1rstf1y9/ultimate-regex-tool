5ba1f295fd7605a0dfc62f881338e16b362af4de
f1rstf1y9 <f1rstf1y9@naver.com>
2025-05-07 18:27:56 +0900

    ➕ Chore: add TailwindCSS dependencies

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,6 +1,6 @@
 const Popup = () => {
   return (
-    <div style={{ padding: "1rem", width: 300 }}>
+    <div>
       <h1>Ultimate Regex Tool</h1>
       <p>Welcome! Start building!</p>
     </div>

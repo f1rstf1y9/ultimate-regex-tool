073026b3b09068721a1d7d8c5860af79200b918f
f1rstf1y9 <f1rstf1y9@naver.com>
2025-05-07 17:46:54 +0900

    🎉 Initial commit
    프로젝트 초기 세팅

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,10 @@
+const Popup = () => {
+  return (
+    <div style={{ padding: "1rem", width: 300 }}>
+      <h1>Ultimate Regex Tool</h1>
+      <p>Welcome! Start building!</p>
+    </div>
+  );
+};
+
+export default Popup;
